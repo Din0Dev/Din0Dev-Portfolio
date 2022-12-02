8464837139061b5a0c9c2f2e572559d41729c7c0
Din0Dev <honghai251.info@gmail.com>
2022-12-02 22:45:22 +0700

    Fix Something Wrong

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
 export default class MyDocument extends Document {
 	render() {
 		return (
@@ -8,7 +7,7 @@ export default class MyDocument extends Document {
 				<Head>
 					<meta charSet="utf-8" />
 					<meta content="IE=edge" />
-					<link rel="shortcut icon" href="/static/assets/favicon.jpg" />
+					<link rel="shortcut icon" href="assets/favicon.ico" />
 				</Head>
 				<body>
 					<Main />
